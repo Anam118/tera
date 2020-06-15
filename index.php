1712fa6839fa6e74910f1b8ce256e6b156f40372
Anam118 <60475080+Anam118@users.noreply.github.com>
2020-06-16 01:45:21 +0530

    semi final

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <html>
 <body>
-  <h1>WOOOOOOOW!!!</h1>
-  <br />
+<h1>My Web</h1>
+<h3>The Dream</h3>
+<br>
 <?php
-  print `ifconfig`
+  $cloudfront_url = `head -n1 origin.txt`;
+  $img_path = "https://".$cloudfront_url."/dream.jpg";
+  echo "<br>";
+  echo "<img src='{$img_path}' width=100 height=100>";
 ?>
 </body>
-</html
+</html>
