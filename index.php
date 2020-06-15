0440b4444dc0e755fb88b4898a962fa591e242ff
Anam118 <60475080+Anam118@users.noreply.github.com>
2020-06-15 23:58:13 +0530

    if

--- index.php
+++ index.php
@@ -3,10 +3,7 @@
   <h1>WOOOOOOOW!!!</h1>
   <br />
 <?php
-   $cloudant_url=`head -n1 path.txt`;
-   $img_path="https://".$cloudant_url."/withvimalsir.jpg";
-   echo "<br>";
-   echo "<img src="${img_path}' width=100 height=100>"
+  print `ifconfig`
 ?>
 </body>
 </html
