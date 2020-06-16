1f9b5295656edd96c330962b22ba03520598de73
Anam118 <60475080+Anam118@users.noreply.github.com>
2020-06-16 06:19:11 +0530

    last

--- index.php
+++ index.php
@@ -4,10 +4,7 @@
 <h3>The Dream</h3>
 <br>
 <?php
-  $cloudfront_url = `head -n1 origin.txt`;
-  $img_path = "https://".$cloudfront_url."/dream.jpg";
-  echo "<br>";
-  echo "<img src='{$img_path}' width=100 height=100>";
+ print'World Tour'
 ?>
 </body>
 </html>
